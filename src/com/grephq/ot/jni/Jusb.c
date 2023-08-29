67173224e000e73f7fda32d08ac8d9e6aafe7253
Olorunfemi-Ojo Tomiwa <ot.server1@outlook.com>
2023-08-29 13:54:11 +0100

    added jni c file

---
+++ src/com/grephq/ot/jni/Jusb.c
@@ -0,0 +1,55 @@
+#include <jni.h>
+#include <stdio.h>
+#include "com_grephq_ot_Jusb.h"
+
+JNIEXPORT void JNICALL Java_com_grephq_ot_Jusb_open(JNIEnv *, jclass) {
+
+}
+
+JNIEXPORT jint JNICALL Java_com_grephq_ot_Jusb_getDeviceAddress(JNIEnv *, jclass) {
+
+}
+
+JNIEXPORT jint JNICALL Java_com_grephq_ot_Jusb_getDeviceList(JNIEnv *, jclass) {
+
+}
+
+JNIEXPORT jint JNICALL Java_com_grephq_ot_Jusb_getDeviceSpeed(JNIEnv *, jclass) {
+
+}
+
+JNIEXPORT jint JNICALL Java_com_grephq_ot_Jusb_getPortNumber(JNIEnv *, jclass) {
+
+}
+
+JNIEXPORT jint JNICALL Java_com_grephq_ot_Jusb_getConfiguration(JNIEnv *, jclass) {
+
+}
+
+JNIEXPORT jstring JNICALL Java_com_grephq_ot_Jusb_getDescriptor(JNIEnv *, jclass) {
+
+}
+
+JNIEXPORT jint JNICALL Java_com_grephq_ot_Jusb_getBusNumber(JNIEnv *, jclass) {
+
+}
+
+JNIEXPORT void JNICALL Java_com_grephq_ot_Jusb_freeDeviceList(JNIEnv *, jclass) {
+
+}
+
+JNIEXPORT void JNICALL Java_com_grephq_ot_Jusb_setConfiguration(JNIEnv *, jclass, jint) {
+
+}
+
+JNIEXPORT void JNICALL Java_com_grephq_ot_Jusb_setDebug(JNIEnv *, jclass, jint) {
+
+}
+
+JNIEXPORT jint JNICALL Java_com_grephq_ot_Jusb_resetDevice(JNIEnv *, jclass) {
+
+}
+
+JNIEXPORT void JNICALL Java_com_grephq_ot_Jusb_close(JNIEnv *, jclass) {
+
+}
